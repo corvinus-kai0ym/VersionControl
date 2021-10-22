3d8ea247482346e2697f44a948df627207be1837
Áron László Makrai <aronmakrai@gmail.com>
2021-10-22 23:26:18 +0200

    5gyak

--- VersionControl/Form1.Designer.cs
+++ VersionControl/Form1.Designer.cs
@@ -1,15 +1,15 @@
 ﻿
-namespace VersionControl
+namespace gyak5
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,109 +24,74 @@ namespace VersionControl
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.lblFirstName = new System.Windows.Forms.Label();
-            this.lblLastName = new System.Windows.Forms.Label();
-            this.txtFirstName = new System.Windows.Forms.TextBox();
-            this.txtLastName = new System.Windows.Forms.TextBox();
-            this.listUsers = new System.Windows.Forms.ListBox();
-            this.btnWrite = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnAdd
+            // dataGridView1
             // 
-            this.btnAdd.Location = new System.Drawing.Point(288, 135);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(253, 36);
-            this.btnAdd.TabIndex = 11;
-            this.btnAdd.Text = "button1";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 450);
+            this.dataGridView1.TabIndex = 0;
             // 
-            // lblFirstName
+            // dataGridView2
             // 
-            this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(288, 83);
-            this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(38, 15);
-            this.lblFirstName.TabIndex = 10;
-            this.lblFirstName.Text = "label2";
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(421, 13);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(394, 207);
+            this.dataGridView2.TabIndex = 1;
             // 
-            // lblLastName
+            // button1
             // 
-            this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(288, 33);
-            this.lblLastName.Name = "lblLastName";
-            this.lblLastName.Size = new System.Drawing.Size(38, 15);
-            this.lblLastName.TabIndex = 9;
-            this.lblLastName.Text = "label1";
-            // 
-            // txtFirstName
-            // 
-            this.txtFirstName.Location = new System.Drawing.Point(353, 83);
-            this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(188, 23);
-            this.txtFirstName.TabIndex = 8;
-            // 
-            // txtLastName
-            // 
-            this.txtLastName.Location = new System.Drawing.Point(353, 30);
-            this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(188, 23);
-            this.txtLastName.TabIndex = 7;
-            // 
-            // listUsers
-            // 
-            this.listUsers.FormattingEnabled = true;
-            this.listUsers.ItemHeight = 15;
-            this.listUsers.Location = new System.Drawing.Point(23, 12);
-            this.listUsers.Name = "listUsers";
-            this.listUsers.Size = new System.Drawing.Size(226, 319);
-            this.listUsers.TabIndex = 6;
-            // 
-            // btnWrite
-            // 
-            this.btnWrite.Location = new System.Drawing.Point(288, 188);
-            this.btnWrite.Name = "btnWrite";
-            this.btnWrite.Size = new System.Drawing.Size(253, 36);
-            this.btnWrite.TabIndex = 12;
-            this.btnWrite.Text = "button2";
-            this.btnWrite.UseVisualStyleBackColor = true;
-            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            this.button1.Location = new System.Drawing.Point(494, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 365);
-            this.Controls.Add(this.btnWrite);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.lblFirstName);
-            this.Controls.Add(this.lblLastName);
-            this.Controls.Add(this.txtFirstName);
-            this.Controls.Add(this.txtLastName);
-            this.Controls.Add(this.listUsers);
+            this.ClientSize = new System.Drawing.Size(827, 475);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Label lblFirstName;
-        private System.Windows.Forms.Label lblLastName;
-        private System.Windows.Forms.TextBox textFirstName;
-        private System.Windows.Forms.TextBox txtLastName;
-        private System.Windows.Forms.ListBox listUsers;
-        private System.Windows.Forms.TextBox txtFirstName;
-        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- VersionControl/Form1.cs
+++ VersionControl/Form1.cs
@@ -1,52 +1,94 @@
-﻿using System;
+﻿using gyak5.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text;ny
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using VersionControl.Entities;
 
-namespace VersionControl
+namespace gyak5
 {
     public partial class Form1 : Form
     {
-        BindingList<User> users = new BindingList<User>();
-        
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
-            lblLastName.Text = Resource.FullName;
-            btnAdd.Text = Resource.Add;
-            btnWrite.Text = Resource.Write;
 
-            // listbox1
-            listUsers.DataSource = users;
-            listUsers.ValueMember = "ID";
-            listUsers.DisplayMember = "FullName";
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
 
-            var u = new User()
+            CreatePortfolio();
+
+            List<decimal> nyereség = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
             {
-                FullName = txtLastName.Text,
-                
-            };
-            users.Add(u);
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                nyereség.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségRendezve = (from x in nyereség
+                                      orderby x
+                                      select x).ToList();
+            MessageBox.Show(nyereségRendezve[nyereségRendezve.Count() / 5].ToString());
         }
 
-        private void btnWrite_Click(object sender, EventArgs e)
+        private void CreatePortfolio()
         {
-            SaveFileDialog sfd = new SaveFileDialog();
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+
             if (sfd.ShowDialog() != DialogResult.OK)
                 return;
+
             using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
-                foreach (var u in users)
+            {
+                sw.WriteLine("Időszak;Nyereség");
+                for (int i = 0; i < nyereségRendezve.Count(); i++)
                 {
-                    sw.Write(u.ID+"-"+ u.FullName);
-                    sw.WriteLine();
+                    sw.WriteLine(string.Format("{0};{1}",
+                        Math.Round((double)i / (double)nyereségRendezve.Count(), 2),
+                        nyereségRendezve[i]));
                 }
+            }
         }
     }
 }
