f38c7b4ae2047de61e5d5ea38fd68f042ba016b0
Áron László Makrai <aronmakrai@gmail.com>
2021-10-29 22:55:07 +0200

    call webservices

--- mnb_kai0ym/Form1.cs
+++ mnb_kai0ym/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,26 @@ namespace mnb_kai0ym
         {
             InitializeComponent();
         }
+
+        void test()
+         {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            
+            var response = mnbService.GetExchangeRatesAsync(request);
+
+           
+            //var result = response.GetExchangeRatesResult;
+
+        }
+        
 
     }
 }
