dfb06fe9fe286560b75b51918c186f3c393d1ed8
Áron László Makrai <aronmakrai@gmail.com>
2021-11-12 22:53:26 +0100

    override

--- porductionLine_kai0ym/Entities/Ball.cs
+++ porductionLine_kai0ym/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using porductionLine_kai0ym.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
@@ -6,29 +7,12 @@ using System.Windows.Forms;
 
 namespace porductionLine_kai0ym.Entities
 {
-    class Ball:Label
+    class Ball:Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = 50;
-            Paint += Ball_Paint;
-        }
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage (Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }
