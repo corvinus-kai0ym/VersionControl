4f2d825cf1a271fa051534948cc811a4558bada7
Áron László Makrai <aronmakrai@gmail.com>
2021-11-12 22:26:47 +0100

    ballfactory

---
+++ porductionLine_kai0ym/Entities/BallFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace porductionLine_kai0ym.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
