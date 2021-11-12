b35a28bf5cbf8cb791b36ddb031ab6c71eb12784
Áron László Makrai <aronmakrai@gmail.com>
2021-11-12 23:02:42 +0100

    car

---
+++ porductionLine_kai0ym/Entities/Car.cs
@@ -0,0 +1,17 @@
+﻿using porductionLine_kai0ym.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace porductionLine_kai0ym.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
