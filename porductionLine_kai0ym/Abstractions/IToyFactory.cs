1b9cc491894d8261414ac9fafb78d2dd06d41b41
Áron László Makrai <aronmakrai@gmail.com>
2021-11-12 23:04:08 +0100

    interface

---
+++ porductionLine_kai0ym/Abstractions/IToyFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace porductionLine_kai0ym.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+
+    }
+}
