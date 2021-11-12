8ce353aeec3a7d90f3dd0759cf24687c3c44e4f7
Áron László Makrai <aronmakrai@gmail.com>
2021-11-12 22:51:34 +0100

    toy

---
+++ porductionLine_kai0ym/Abstractions/Toy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace porductionLine_kai0ym.Abstractions
+{
+   public abstract class Toy:Label
+    {
+
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+        
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
